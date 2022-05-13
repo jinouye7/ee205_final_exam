1070413764248c1b5905a6e3f7a19a36b7f83248
jinouye7 <jinouye7@hawaii.edu>
2022-05-13 09:59:35 -1000

    skeleton for shape class

---
+++ shape.cpp
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file shape.cpp
+/// @version 1.0
+///
+/// @author Jared Inouye <jinouye7@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "shape.h"
+
+
---
+++ shape.h
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_final_exam - EE 205 - Spr 2022
+///
+/// @file shape.h
+/// @version 1.0
+///
+/// @author Jared Inouye <jinouye7@hawaii.edu>
+/// @date   13_May_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef EE205_FINAL_EXAM_SHAPE_H
+#define EE205_FINAL_EXAM_SHAPE_H
+#include <iostream>
+
+
+class shape {
+public:
+    virtual double computeArea() = 0;
+
+
+};
+
+
+#endif //EE205_FINAL_EXAM_SHAPE_H
